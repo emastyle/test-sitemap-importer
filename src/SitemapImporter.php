d5ab3e223b7308e8b707659ff163dd537c5b8d55
Emanuele Gian <emanuele.gian@quinary.com>
2020-05-05 17:14:04 +0200

    Add logic to import and parse sitemap

--- src/SitemapImporter.php
+++ src/SitemapImporter.php
@@ -1,7 +1,31 @@
 <?php
+
 namespace SitemapImporter;
 
+/**
+ * Class SitemapImporter
+ * @package SitemapImporter
+ */
 class SitemapImporter
 {
+    /**
+     * @param $source
+     * @return array
+     */
+    public function ImportSitemap($source)
+    {
+        $content = file_get_contents($source);
+        $sitemapXml = simplexml_load_string($content);
 
+        if (empty($sitemapXml)) {
+            return [];
+        }
+
+        $mapData = [];
+        foreach ($sitemapXml->sitemap as $sitemapElement) {
+            $url = parse_url((string)$sitemapElement->loc);
+            $mapData[$url['host']][] = ltrim($url['path'], '/');
+        }
+        return $mapData;
+    }
 }
